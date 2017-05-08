c5c34cb32d84820aeeb52a2e9dd417b27154cea9
DanielRhazen <dma.morais29@gmail.com>
2017-05-08 02:48:52 -0300

    Criação da função que retorna os projetos classificados como de risco.

--- repository/projects/index.js
+++ repository/projects/index.js
@@ -1,5 +1,7 @@
 const projects = require('./data').projects;
 
+const projectsAtRisk = [];
+
 function create(project) {
   if (project.name !== '' && project.name !== undefined) {
     projects.push(project);
@@ -12,4 +14,32 @@ function findAll() {
   return projects;
 }
 
-module.exports = { create, findAll };
+function findProjectsAtRisk() {
+
+  let yellow;
+  let red;
+
+  for(var i = 0; i < projects.length; i++){
+
+    red = 0;
+    yellow = 0;
+
+    for(var j = 0; j < projects[i].reports.length; j++){
+
+      if(projects[i].reports[j] === 'Yellow'){
+        yellow++;
+      }
+      
+      if(projects[i].reports[j] === 'Red'){
+        red++;
+      }
+    }
+
+    if(red >= 1 || yellow >= 2){
+      projectsAtRisk.push(projects[i]);
+    }
+  }
+  return projectsAtRisk;
+}
+
+module.exports = { create, findAll, findProjectsAtRisk };
