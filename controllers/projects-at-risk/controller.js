267d085d4e0fdaf0132093644bcde3303939647c
DanielRhazen <dma.morais29@gmail.com>
2017-05-08 03:01:22 -0300

    Status HTTP

--- controllers/projects-at-risk/controller.js
+++ controllers/projects-at-risk/controller.js
@@ -2,7 +2,7 @@ const projectsService = require('../../services/projects');
 
 function get(req, res) {
   const projects = projectsService.findProjectsAtRisk();
-  return res.json(projects);
+  return res.status(200).json(projects);
 }
 
 module.exports = { get };
--- controllers/projects/controller.js
+++ controllers/projects/controller.js
@@ -2,14 +2,14 @@ const projectsService = require('../../services/projects');
 
 function get(req, res) {
   const projects = projectsService.findAll();
-  return res.json(projects);
+  return res.status(200).json(projects);
 }
 
 function post(req, res) {
   if (projectsService.create(req.body)){
     return res.status(201).json({ message: 'Project Created' });  
   } else {
-    return res.json({ message: 'Missing ProjectName' });
+    return res.status(400).json({ message: 'Missing ProjectName' });
   }
 }
 
